d8da2c3be765f05850a42d34762a3e6142585e1f
Manish Pramod Patil <manish4487@gmail.com>
2017-09-08 10:58:13 -0500

    Add files via upload

---
+++ Tic_Tac_Toe/Tic_Tac_Toe/JavaScript.js
@@ -0,0 +1,29 @@
+﻿//resart button for game
+var restart = document.querySelector("#b");
+//select all the squares in the game
+var squares = document.querySelectorAll('td');
+
+function clearsquares() {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].textContent = '';
+    }
+}
+
+restart.addEventListener('click', clearsquares);
+
+//check on square
+function cycleMark() {
+    if (this.textContent === '') {
+        this.textContent = 'X';
+    } else if (this.textContent) {
+        this.textContent = 'O';
+    } else {
+        this.textContent = '';
+    }
+}
+
+//add eventlisterner to all squares
+
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', cycleMark)
+}
